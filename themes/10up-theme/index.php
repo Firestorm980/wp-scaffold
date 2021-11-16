133fa88e7aeb496c6385aa5b8a9f4403f09521df
Jon Christensen <jon.christensen@10up.com>
2021-11-16 14:23:47 -0600

    add some utility classes

--- themes/10up-theme/index.php
+++ themes/10up-theme/index.php
@@ -7,12 +7,16 @@
 
 get_header(); ?>
 
-	<?php if ( have_posts() ) : ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<h2><?php the_title(); ?></h2>
-			<?php the_content(); ?>
-		<?php endwhile; ?>
-	<?php endif; ?>
+<main>
+	<div class="container mx-auto">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+				<?php the_content(); ?>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</main>
 
 <?php
 get_footer();
